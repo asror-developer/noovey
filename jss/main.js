d5569321a765e04def3c3831961011c7594cadf1
asror-developer <asroreshnazarov8@gmail.com>
2024-05-21 10:49:46 +0500

    novey

---
+++ jss/main.js
@@ -0,0 +1,44 @@
+ const userrlist =document.getElementById('userlist')
+//  fetch('https://jsonplaceholder.typicode.com/users')
+//       .then(response => response.json())
+//     //   .then(json => console.log(json))   
+//     .then((users)=>{
+//         users.map((user,index)=>{
+//             const name =document.createElement('li')
+//             const email =document.createElement('p')
+//             const number =document.createElement('p')
+//             email.textContent =user.email;
+//             name.textContent =user.name;
+//             number.textContent = index+1
+//             userrlist.appendChild(number)
+//             userrlist.appendChild(name)
+//             userrlist.appendChild(email)
+//         })
+//     })
+//     .catch((error)=>{
+//         console.log("xatolik", error);
+//     })
+
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//       .then(response => response.json())
+//       .then((posts)=>{
+//         posts.map((posts) =>{
+//             // const id = document.createElement('li')
+//             const body = document.createElement('p')
+//             // id.textcontent=posts.id;
+//             body.textContent=posts.body;
+//             // userrlist.appendChild(id)
+//             userrlist.appendChild(body)
+          
+//         })
+//       })
+//       .catch((error)=>{
+//          console.log("xatolik chiqar",error);
+//       })
+
+
+
+
+// tokenlar-----------------------------------------------------
+
+  
